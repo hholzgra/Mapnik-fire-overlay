58d9c03317b03268f6254a5a5e96a7e3b2ed8966
Hartmut Holzgraefe <hartmut@php.net>
2018-04-21 21:29:35 +0000

    make diameter and volume text color settable per country too

--- sql-functions/fire_symbols.sql
+++ sql-functions/fire_symbols.sql
@@ -8,3 +8,16 @@ EXCEPTION WHEN OTHERS THEN
    RETURN 'symbols';
 END
 $$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION fire_symbol_color() RETURNS TEXT AS $$
+BEGIN
+  CASE current_setting('mapnik.country')
+    WHEN 'CH' THEN RETURN 'blue';
+    ELSE           RETURN 'red';
+  END CASE;
+EXCEPTION WHEN OTHERS THEN
+   RETURN 'red';
+END
+$$ LANGUAGE plpgsql;
+
+
